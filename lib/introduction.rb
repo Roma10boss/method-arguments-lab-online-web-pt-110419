21403f5c42ae1b97f1a1afda0dafd804fd0a5713
Romario Gustave <romagustave10@gmail.com>
2019-11-06 06:31:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,4 +6,5 @@ end
 def introduction(name, language)
   name = "Dan"
   language = "ember.js"
-   puts "Hi, my name is "+ name + " and I am "
+   puts "Hi, my name is "+ name + " and I am  learning to program in" + language"
+ end
